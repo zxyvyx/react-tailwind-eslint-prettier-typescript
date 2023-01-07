d6eb5530f0037619ef0db7dbad7eeb606e592444
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2023-01-07 13:34:54 +0700

    chore(git): initial react + typescript + tailwindcss project
    Signed-off-by: Hasymi Al <68429761+zxyvyx@users.noreply.github.com>

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-teal-500 font-bold underline">
+        React + Tailwind CSS + ESLint + Prettier + TypeScript
+      </h1>
+    </>
+  );
+}
